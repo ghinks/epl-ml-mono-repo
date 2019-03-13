4dc7a3ecc17685e81a240636db9b46b189ba9c3c
Glenn Hinks <ghinks@yahoo.com>
2019-03-13 15:55:08 -0400

    basic layout with two packages

---
+++ packages/integration-tests/index.js
@@ -0,0 +1,5 @@
+const findMatchesInPath = require("@gvhinks/epl-data-reader").default;
+
+(async function tester() {
+  await findMatchesInPath("/Users/ghinks/dev/match-analysis/packages/epl-data-reader/data");
+})();
