638de70d5d444ad9ef0b6ede5d07fab9cdfa4311
Glenn Hinks <ghinks@yahoo.com>
2019-03-17 19:27:59 -0400

    map the given data properties to more descriptive props in the DB.

--- packages/epl-data-to-db/src/index.ts
+++ packages/epl-data-to-db/src/index.ts
@@ -4,13 +4,66 @@ import {MatchResult} from "@gvhinks/epl-data-reader";
 
 const url = 'mongodb://localhost:27017';
 const dbName = 'epl-scores';
+
+interface MatchData{
+  awayTeamCorners: number;
+  awayTeamFouls: number;
+  awayTeamReds: number;
+  awayTeamShots: number;
+  awayTeamShotsOnTarget: number;
+  awayTeamYellowCards: number;
+  AwayTeam: string;
+  Date: string;
+  fullTimeAwayGoals: number;
+  fullTimeHomeGoals: number;
+  fullTimeResult: string;
+  homeTeamCorners: number;
+  homeTeamFouls: number;
+  homeTeamRedCards: number;
+  homeTeamShots: number;
+  homeTeamShotsOnTarget: number;
+  halfTimeAwayTeamGoals: number;
+  halfTimeHomeTeamGoals: number;
+  halfTimeResult: string;
+  homeTeamYellowCards: number;
+  homeTeam: string;
+  referee: string;
+};
+
+const renameProps = (games: MatchResult[]): MatchData[] => {
+  return games.map(g => ({
+    awayTeamCorners: g.AC,
+    awayTeamFouls: g.AF,
+    awayTeamReds: g.AR,
+    awayTeamShots: g.AS,
+    awayTeamShotsOnTarget: g.AST,
+    awayTeamYellowCards: g.AY,
+    AwayTeam: g.AwayTeam,
+    Date: g.Date,
+    fullTimeAwayGoals: g.FTAG,
+    fullTimeHomeGoals: g.FTHG,
+    fullTimeResult: g.FTR,
+    homeTeamCorners: g.HC,
+    homeTeamFouls: g.HF,
+    homeTeamRedCards: g.HR,
+    homeTeamShots: g.HS,
+    homeTeamShotsOnTarget: g.HST,
+    halfTimeAwayTeamGoals: g.HTAG,
+    halfTimeHomeTeamGoals: g.HTHG,
+    halfTimeResult: g.HTR,
+    homeTeamYellowCards: g.HY,
+    homeTeam: g.HomeTeam,
+    referee: g.Referee
+  }));
+};
+
 const writeToDB = async (games: MatchResult[]): Promise<boolean> => {
   try {
     const client: MongoClient = await MongoClient.connect(url, { useNewUrlParser: true });
     console.info("connected...");
     const matches = await client.db(dbName).createCollection("matches");
     console.log(matches);
-    await matches.insertMany(games);
+    await matches.insertMany(renameProps(games));
     client.close();
     console.info("closed matches");
     return true;
