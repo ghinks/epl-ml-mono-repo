1575ca99430d246cbb58a45d2526feab59402b65
Glenn Hinks <ghinks@yahoo.com>
2019-03-17 17:25:18 -0400

    add readme key tidy up integration test

--- packages/epl-data-to-db/src/index.ts
+++ packages/epl-data-to-db/src/index.ts
@@ -2,17 +2,13 @@ import { MongoClient, Db } from "mongodb";
 import assert from "assert";
 import {MatchResult} from "@gvhinks/epl-data-reader";
 
-// Connection URL
 const url = 'mongodb://localhost:27017';
-
-// Database Name
 const dbName = 'epl-scores';
-//
 const writeToDB = async (games: MatchResult[]): Promise<boolean> => {
   try {
     const client: MongoClient = await MongoClient.connect(url, { useNewUrlParser: true });
     console.info("connected...");
-    const matches = await client.db("epl").createCollection("matches");
+    const matches = await client.db(dbName).createCollection("matches");
     console.log(matches);
     await matches.insertMany(games);
     client.close();
