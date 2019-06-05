2da7e19b1aeeb0d0638e6517393866aa8f86cd58
Glenn Hinks <ghinks@yahoo.com>
2019-06-05 07:42:48 -0400

    remove commented out code

--- packages/epl-base-model/src/index.ts
+++ packages/epl-base-model/src/index.ts
@@ -8,7 +8,6 @@ const createModel = async (): Promise<tf.Sequential> => {
   model.add(tf.layers.dense({inputShape: [2,36], units: 3, useBias: true, name:"teams_layer"}));
   model.add(tf.layers.flatten());
   model.add(tf.layers.dense({units: 3, useBias: true, name: "results_layer"}));
-  // model.summary();
   model.compile({optimizer: tf.train.adam(0.001), loss: 'meanSquaredError'});
   const { labelValues, featureValues } = await getTrainingData();
   const numTeamsInLeague: number = (featureValues[0][0]).length;
@@ -18,30 +17,10 @@ const createModel = async (): Promise<tf.Sequential> => {
     batchSize: 500,
     epochs: 200,
     verbose: 0
-    /*
-    callbacks: {
-      onEpochEnd: (epoch, logs): void => {
-        console.log(Array(20).join("*"));
-        console.log("my epoch ended")
-        console.log(epoch)
-        console.log(logs)
-      },
-      onBatchEnd: (batch, logs): void => {
-        console.log(Array(20).join("*"));
-        console.log("my batch ended")
-        console.log(batch)
-        console.log(logs)
-      }
-    }
-     */
   };
-  // const history =
   await model.fit(featureTensors, labelTensors, fitArgs);
-  // console.log(history);
-  // const Arsenal = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   const Chelsea = [0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   const WestHam = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0];
-  // const Everton = [0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
   const testData = tf.tensor3d([ ...Chelsea, ...WestHam], [1,2,36], 'int32');
   const result = model.predict(testData);
   console.log(Array(100).join("="));
