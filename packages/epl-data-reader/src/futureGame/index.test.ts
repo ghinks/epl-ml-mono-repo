d30d7c02aa7bd36a704358f1437140d7f1ae820b
Glenn Hinks <ghinks@yahoo.com>
2019-07-06 12:14:20 -0400

    fix the date issue for conversion and add a check on all the dates that come back from the conversion to check that are at least in the correct football season.

--- packages/epl-data-reader/src/futureGame/index.test.ts
+++ packages/epl-data-reader/src/futureGame/index.test.ts
@@ -4,5 +4,11 @@ describe("Get future games", (): void => {
   test("expect to retrieve future games", async (): Promise<void> => {
     const games: FutureGame[] = await getFutureGames();
     expect(games.length).toBeGreaterThan(1);
+    const minDate = new Date("2019/01/01");
+    games.forEach((game: FutureGame) => {
+      const result = game.date > minDate;
+      if (!result) console.error(`expected ${game.date} > ${minDate} for ${game.homeTeam}`);
+      expect(result).toBeTruthy();
+    });
   });
 });
--- packages/epl-data-reader/src/futureGame/index.ts
+++ packages/epl-data-reader/src/futureGame/index.ts
@@ -13,9 +13,10 @@ const getFutureData = async (): Promise<FutureGame[]> => {
   const fileName: string = path.join(__dirname, "../../data/futureFixtures/eplSeason2020.json");
   const rawData: object[] = JSON.parse(await fs.promises.readFile(fileName, "utf-8"));
   const fixtures: FutureGame[] = rawData.map((data): FutureGame => {
+    const match = data["Date"].match(/(\d+)\/(\d+)\/(\d+).*/);
     return {
       roundNumber: data["Round Number"],
-      date: new Date(data["Date"]),
+      date: new Date(match[3], match[2], match[1]),
       location: data["Location"],
       homeTeam: data["Home Team"],
       awayTeam: data["Away Team"]
