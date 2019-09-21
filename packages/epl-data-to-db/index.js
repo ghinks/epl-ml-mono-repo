7034c655b73c6fc79d20dcafed508a9ae1b9b847
Glenn Hinks <ghinks@yahoo.com>
2019-09-21 09:48:32 -0400

    remove one of the todos as we now have the correct season estimation

--- packages/epl-data-to-db/index.js
+++ packages/epl-data-to-db/index.js
@@ -3,30 +3,6 @@ const db = require("./dist/index");
 const { getFixtures, default: getHistoricalData} = reader;
 const { writeFutureFixtures, default: writeHistoricalData, updateFeatures } = db;
 
-
-// TODO seasons not quite correct
-/*
-{
-  "_id" : 8,
-  "firstGame" : ISODate("2008-09-16T04:00:00.000Z"),
-  "lastGame" : ISODate("2009-08-16T00:00:00.000Z"),
-  "total" : 781.0
-}
-
-{
-  "_id" : 9,
-  "firstGame" : ISODate("2009-08-18T00:00:00.000Z"),
-  "lastGame" : ISODate("2010-05-09T00:00:00.000Z"),
-  "total" : 741.0
-}
-
-{
-  "_id" : 18,
-  "firstGame" : ISODate("2018-08-10T00:00:00.000Z"),
-  "lastGame" : ISODate("2019-03-03T00:00:00.000Z"),
-  "total" : 579.0
-}
- */
 // TODO Documuent kaggle seasons data
 console.log("starting db import from static data files");
 
